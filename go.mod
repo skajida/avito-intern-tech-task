4e59abd2022d30da1620ca07a4c12f6abdada889
Fedor Korolev <thiranger@yandex.ru>
2023-08-29 08:25:41 +0300

    implemented error response form

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module service-segs
 
 go 1.20
+
+require github.com/google/uuid v1.3.1
---
+++ internal/model/http-response-forms/error.go
@@ -0,0 +1,36 @@
+package httpresponseforms
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type ErrorResponse struct {
+	Title        string `json:"title"`
+	Request      string `json:"request"`
+	Time         string `json:"time"`
+	ErrorTraceId string `json:"errorTraceId"`
+}
+
+func NewErrorResponse(r *http.Request, title string) *ErrorResponse {
+	return &ErrorResponse{
+		Title:        title,
+		Request:      fmt.Sprint(r.Method, " ", r.URL),
+		Time:         time.Now().Format(time.RFC3339),
+		ErrorTraceId: uuid.New().String(),
+	}
+}
+
+func (this *ErrorResponse) Send(w http.ResponseWriter, status int) {
+	defer log.Println("Responded with errorTraceId", this.ErrorTraceId)
+
+	json_representation, _ := json.Marshal(this)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(json_representation)
+}
