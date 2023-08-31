34d4d6e160fcd969d54d7a72ddb7f561b3a610f6
Fedor Korolev <thiranger@yandex.ru>
2023-08-31 11:10:20 +0300

    service initialization

--- cmd/service-segs/main.go
+++ cmd/service-segs/main.go
@@ -1,25 +1,38 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
-	"service-segs/internal/handler"
+	"service-segs/internal/handler/belonging"
+	"service-segs/internal/handler/segments"
+	"service-segs/internal/repository"
+	"service-segs/internal/service"
 	"time"
 )
 
 func main() {
-	mux := http.NewServeMux()
-	mux.Handle("/segs", &handler.SegmentsHandler{})
-	mux.Handle("/segs/", &handler.UserHandler{})
+	if database, err := sql.Open("postgres", "user=postgres password=root sslmode=disable"); err != nil {
+		log.Fatalln(err)
+	} else {
+		externalRepository := repository.NewERepository(&struct{}{})
+		internalRepository := repository.NewIRepository(database)
 
-	server := &http.Server{
-		Addr:           ":8080",
-		Handler:        mux,
-		ReadTimeout:    time.Second,
-		WriteTimeout:   time.Second,
-		MaxHeaderBytes: 1 << 16,
+		serviceSegs := service.NewSegmentsService(externalRepository, internalRepository)
+
+		muxer := http.NewServeMux()
+		muxer.Handle("/segs", segments.NewHandler(serviceSegs))
+		muxer.Handle("/users/", belonging.NewHandler(serviceSegs))
+
+		server := &http.Server{
+			Addr:           ":8080",
+			Handler:        muxer,
+			ReadTimeout:    10 * time.Second,
+			WriteTimeout:   10 * time.Second,
+			MaxHeaderBytes: 1 << 16,
+		}
+
+		log.Println("Starting server at", server.Addr, "port")
+		log.Fatalln(server.ListenAndServe())
 	}
-
-	log.Println("Starting server at", server.Addr, "port")
-	log.Fatalln(server.ListenAndServe())
 }
