d0ddfe9f957c6663fba3c009c94a2503378d73ae
Fedor Korolev <thiranger@yandex.ru>
2023-08-31 10:56:00 +0300

    user db mock

---
+++ internal/repository/external.go
@@ -0,0 +1,22 @@
+package repository
+
+import "context"
+
+const totalEntries = 10_000
+
+// dummy emulation of the behaviour of an external service containing users database
+type ERepository struct {
+	database any
+}
+
+func NewERepository(users any) *ERepository {
+	return &ERepository{database: users}
+}
+
+func (*ERepository) Exists(ctx context.Context, id int) bool {
+	return id <= totalEntries
+}
+
+func (*ERepository) Count(context.Context) int {
+	return totalEntries
+}
