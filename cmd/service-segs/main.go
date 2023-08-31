5998d682f6422720b151576e3df3e8f6538f491b
Fedor Korolev <thiranger@yandex.ru>
2023-08-31 17:10:28 +0300

    changed initialization

--- cmd/service-segs/main.go
+++ cmd/service-segs/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"service-segs/internal/handler/belonging"
+	"service-segs/internal/handler/history"
 	"service-segs/internal/handler/segments"
 	"service-segs/internal/repository"
 	"service-segs/internal/service"
@@ -12,27 +13,31 @@ import (
 )
 
 func main() {
-	if database, err := sql.Open("postgres", "user=postgres password=root sslmode=disable"); err != nil {
+	database, err := sql.Open("postgres", "user=postgres password=root sslmode=disable")
+	if err != nil {
 		log.Fatalln(err)
-	} else {
-		externalRepository := repository.NewERepository(&struct{}{})
-		internalRepository := repository.NewIRepository(database)
-
-		serviceSegs := service.NewSegmentsService(externalRepository, internalRepository)
-
-		muxer := http.NewServeMux()
-		muxer.Handle("/segs", segments.NewHandler(serviceSegs))
-		muxer.Handle("/users/", belonging.NewHandler(serviceSegs))
-
-		server := &http.Server{
-			Addr:           ":8080",
-			Handler:        muxer,
-			ReadTimeout:    10 * time.Second,
-			WriteTimeout:   10 * time.Second,
-			MaxHeaderBytes: 1 << 16,
-		}
-
-		log.Println("Starting server at", server.Addr, "port")
-		log.Fatalln(server.ListenAndServe())
 	}
+
+	externalRepository := repository.NewERepository(&struct{}{})
+	internalRepository := repository.NewIRepository(database)
+
+	serviceSegs := service.NewSegmentsService(externalRepository, internalRepository)
+
+	muxer := http.NewServeMux()
+	muxer.Handle("/segs", segments.NewHandler(serviceSegs))
+	muxer.Handle("/users/", belonging.NewHandler(serviceSegs))
+	bonus1 := history.NewHandler(serviceSegs)
+	muxer.Handle("/history/", bonus1)
+	muxer.Handle("/download/", bonus1)
+
+	server := &http.Server{
+		Addr:           ":8080",
+		Handler:        muxer,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 16,
+	}
+
+	log.Println("Starting server at", server.Addr, "port")
+	log.Fatalln(server.ListenAndServe())
 }
